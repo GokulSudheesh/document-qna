8bd69b4f9726de44ab727c047405ba6027054637
Gokul Sudheesh <gokulsudheesh1@gmail.com>
2025-06-21 22:45:06 +0400

    chore: update chat sys prompt

--- doc-summarizer-be/app/core/chain/prompts.py
+++ doc-summarizer-be/app/core/chain/prompts.py
@@ -5,6 +5,7 @@ def get_chat_prompt_template() -> ChatPromptTemplate:
     system_template = """ 
     - You are an assistant for question-answering tasks.
     - Use the following pieces of retrieved context and user information to answer the question. Please do not make up answers or answer out of context.
+    - If the context is not sufficient to answer the question, then say "I don't have that information at hand. Please provide more details or check your sources.", this is very important.
     - If you don't find the answer of the query,then just say I don't have that information at hand. Please provide more details or check your sources.
     
     {context}
---
+++ doc-summarizer-be/tests/index-docs.py
@@ -0,0 +1,41 @@
+import asyncio
+from uuid import uuid4
+from core.indexers.qdrant import doc_indexer
+from utils.qdrant import get_chat_response
+import os
+
+
+docs_dir = "tests/docs"
+
+
+def read_txt_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        return f.read()
+
+
+async def index_docs():
+    # session_id = uuid4()
+    session_id = "92e3b302-36d6-44c0-83a1-6ce89adae2b7"
+    for filename in os.listdir(docs_dir):
+        file_path = os.path.join(docs_dir, filename)
+        if os.path.isfile(file_path):
+            extracted_text = read_txt_file(file_path)
+            await doc_indexer.index_documents(
+                extracted_text=extracted_text,
+                meta_data={
+                    "file_id": uuid4(),
+                    "session_id": session_id,
+                    "file_name": filename,
+                    "file_type": "text/plain"
+                }
+            )
+
+
+async def main():
+    await index_docs()
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        print(f"An error occurred: {e}")
